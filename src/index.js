7f8d672ce2c80732b46cebad554b7bc1b993bff7
GLEB PROKOPOVICH <fanta_23@mail.ru>
2019-03-01 23:02:43 +0300

    finalized index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,18 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let effect=0;
+  for (let a = 0; a < preferences.length; a++){
+      let up1 = preferences[a]-1;
+      if (up1==a) continue;
+      let up2 = preferences[up1]-1;
+      if (up1==up2) continue;
+      let up3 = preferences[up2]-1;
+      if (up3==up2) continue;
+      if (up3==a){effect++};
+      
+  };
+    return effect/3 ;
+    
 };
+
